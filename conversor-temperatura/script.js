153173185f3e1f547fe37f8ff4dd9f5fc5ac1c0f
Joao Pedro Cadinelli <jpcadinelli@gmail.com>
2023-10-10 23:27:02 -0300

    Projeto finalizado

--- conversor-temperatura/script.js
+++ conversor-temperatura/script.js
@@ -1,12 +1,24 @@
 const btnFahr = document.querySelector('#fahr')
 const btnCels = document.querySelector('#cels')
-const temp = document.querySelector('#temp')
-const valorTemp = temp.value
 
 let resp = null
 
+function pegaTemp() {
+    const temp = document.querySelector('#temp')
+    const valorTemp = temp.value
+    return valorTemp
+}
+
 btnFahr.addEventListener('click', () => {
     // (0 °C × 9/5) + 32 = 32 °F
-    resp = (valorTemp * 9 / 5) + 32
-    document.querySelector('#resp').innerHTML =`${resp}`
+    var temp = pegaTemp()
+    resp = (temp * 9 / 5) + 32
+    document.querySelector('#resp').innerHTML =`A temperatura é ${resp}°F.`
+})
+
+btnCels.addEventListener('click', () => {
+    // (0 °F -32) x 5/9 = -17,77 °C
+    var temp = pegaTemp()
+    resp = (temp - 32) * 5 / 9
+    document.querySelector('#resp').innerHTML =`A temperatura é ${resp}°C.`
 })
