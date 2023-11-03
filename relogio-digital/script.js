66566b11b673fa8a545fe27c0433e3c54db04b0c
JPCadinelli <jpcadinelli@gmail.com>
2023-11-02 21:44:25 -0300

    projeto funcional

--- relogio-digital/script.js
+++ relogio-digital/script.js
@@ -2,9 +2,27 @@ const getTime = () => {
     const date = new Date
     return {
         hours: date.getHours(),
-        minute: date.getMinutes(),
-        second: date.getSeconds(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
     }
 }
 
-console.log(getTime())
+setInterval(() => {
+    const { hours, minutes, seconds } = getTime()
+
+    if (hours < 10) {
+        document.querySelector('.hora').innerHTML = '0' + hours
+    } else {
+        document.querySelector('.hora').innerHTML = hours
+    }
+    if (minutes < 10) {
+        document.querySelector('.minuto').innerHTML = '0' + minutes
+    } else {
+        document.querySelector('.minuto').innerHTML = minutes
+    }
+    if (seconds < 10) {
+        document.querySelector('.segundo').innerHTML = '0' + seconds
+    } else {
+        document.querySelector('.segundo').innerHTML = seconds
+    }
+}, 1000)
