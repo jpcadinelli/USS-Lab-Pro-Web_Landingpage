e1ac6ad1ae4f08d54e51f211a3960167bc10d55f
JPCadinelli <jpcadinelli@gmail.com>
2023-11-12 15:37:36 -0300

    fazendo com objeto dentro do javascript

--- quiz-interativo/script.js
+++ quiz-interativo/script.js
@@ -1,29 +1,35 @@
 const quizDiv = document.querySelector('#quiz');
 
+const perguntasObj = [
+    {
+        "p": "Como podemos definir um Algoritmo?",
+        "a": "Sequência de comandos ordenados, infinitos e ambíguos que são empregados para executar uma tarefa.",
+        "b": "Sequência de instruções ordenadas, infinitas e não-ambíguas que são empregadas para executar uma tarefa.",
+        "c": "Sequência de instruções ordenadas, finitas e não-ambíguas que são empregadas para executar uma tarefa.",
+        "d": "Sequência de comandos ordenados, infinitos e não-ambíguos que são empregados para executar uma tarefa.",
+        "e": "Sequência de comandos desordenados, infinitos e não-ambivalentes que são empregados para executar uma tarefa.",
+        "r": "c"
+    },
+    {
+        "p": "Qual dos seguintes itens possui apenas tipos válidos usados em lógica de programação (tipos primitivos)?",
+        "a": "Inteiro, Temporal, Caractere, Double",
+        "b": "Booleano, Real, Inteiro, Irracional",
+        "c": "Inteiro, Booleano, Tipografia, Double",
+        "d": "Inteiro, Booleano, Caractere, Double",
+        "e": "Temporal, Triple, Caractere, Double",
+        "r": "d"
+    },
+    {
+        "p": "Quais são todos os estados possíveis que um tipo lógico pode assumir?",
+        "a": "Verdadeiro, Duvidoso",
+        "b": "Falso, Duvidoso, Verdadeiro",
+        "c": "Incerto, Certo",
+        "d": "Verdadeiro, Falso",
+        "e": "Verdadeiro, Errado",
+        "r": "d"
+    }
+]
+
 function iniciarQuiz() {
-    const tema = document.querySelector('#tema')
-    carregarJson(tema)
-}
-
-function carregarJson(tema) {
-    fetch('quiz.json')
-        .then(response => response.json())
-        .then(perguntas => {
-            perguntas.map(pergunta => {
-                const card = document.createElement('div')
-                card.classList.add('perguntas')
-
-                const p = document.createElement('p')
-                p.classList.add('pergunta')
-                p.textContent = `quiz.${tema}.p`
-
-                const respostas = document.createElement('ul')
-                const a = document.createElement('li')
-
-                card.appendChild(p)
-                card.appendChild(a)
-
-                quizDiv.appendChild(card)
-            })
-        })
+    
 }
