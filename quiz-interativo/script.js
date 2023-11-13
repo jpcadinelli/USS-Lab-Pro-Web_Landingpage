7adc981c399b4ca4de3ae77e04ac98e36b0b5cfc
jpcadinelli <jpcadinelli@gmail.com>
2023-11-13 14:02:15 -0300

    projeto pronto

--- quiz-interativo/script.js
+++ quiz-interativo/script.js
@@ -1,6 +1,8 @@
 const quizDiv = document.querySelector('#quiz')
 const main = document.querySelector('#main')
 const resultadoDiv = document.querySelector('.resultado')
+const resultado = document.querySelector('#resultado')
+const tamanho = document.querySelector('#tamanho')
 const p = document.querySelector('.pergunta')
 const a = document.querySelector('.a')
 const b = document.querySelector('.b')
@@ -13,7 +15,8 @@ let resp = null
 let pontos = 0
 
 const perguntasObj = [
-    {
+    {   
+        "id": "1",
         "p": "Como podemos definir um Algoritmo?",
         "a": "Sequência de comandos ordenados, infinitos e ambíguos que são empregados para executar uma tarefa.",
         "b": "Sequência de instruções ordenadas, infinitas e não-ambíguas que são empregadas para executar uma tarefa.",
@@ -23,6 +26,7 @@ const perguntasObj = [
         "r": "c"
     },
     {
+        "id": "2",
         "p": "Qual dos seguintes itens possui apenas tipos válidos usados em lógica de programação (tipos primitivos)?",
         "a": "Inteiro, Temporal, Caractere, Double",
         "b": "Booleano, Real, Inteiro, Irracional",
@@ -32,12 +36,83 @@ const perguntasObj = [
         "r": "d"
     },
     {
+        "id": "3",
         "p": "Quais são todos os estados possíveis que um tipo lógico pode assumir?",
         "a": "Verdadeiro, Duvidoso",
         "b": "Falso, Duvidoso, Verdadeiro",
         "c": "Incerto, Certo",
         "d": "Verdadeiro, Falso",
         "e": "Verdadeiro, Errado",
+        "r": "d"
+    },
+    {
+        "id": "4",
+        "p": "Qual dos seguintes itens é melhor representado por um valor constante em um algoritmo?",
+        "a": "Salário de um Funcionário",
+        "b": "Valor da Temperatura no final de semana",
+        "c": "Valor matemático de Pi",
+        "d": "Distância da Terra à Lua",
+        "e": "Velocidade de um carro em movimento",
+        "r": "c"
+    },
+    {
+        "id": "5",
+        "p": "É possível ordenar valores numéricos já armazenados em um vetor de números decimais?",
+        "a": "Não, somente é possível ter valores em ordem se os valores forem inseridos em ordem",
+        "b": "Sim, mas somente se os números forem inteiros. Números decimais não podem ser ordenados",
+        "c": "Sim, mas para isso é necessário usar um algoritmo para ordenação de vetores",
+        "d": "Vetores não permitem o armazenamento de números decimais, somente números reais",
+        "e": "Não, não é possível ordenar valores já armazenados em um vetor em nenhuma hipótese",
+        "r": "c"
+    },
+    {
+        "id": "6",
+        "p": "Quais são os tipos de arrays que podemos empregar na criação de algoritmos?",
+        "a": "Unidimensional (linha), Bidimensional (vetor), Tridimensional (cubo)",
+        "b": "Dimensional (com dimensões), Adimensional (sem dimensões), Paradimensional (com e sem dimensões)",
+        "c": "Unidimensional (vetor), Bidimensional (quadrado), Tridimensional (esfera)",
+        "d": "Unidimensional (vetor), Bidimensional (matriz), Tridimensional (cubo)",
+        "e": "Unidimensional (ponto), Bidimensional (matriz), Tridimensional (campo)",
+        "r": "d"
+    },
+    {
+        "id": "7",
+        "p": "O que é Compilação?",
+        "a": "Comandos contidos no código-fonte são executados à medida em que são traduzidos, sendo necessária a interpretação toda vez que se quiser rodar o programa.",
+        "b": "Tradução do código-fonte em um ou mais arquivos que podem ser armazenados e executados quantas vezes se desejar, sem necessitar de nova compilação.",
+        "c": "Junção de comandos do código-fonte em um arquivo empacotado para distribuição por um meio específico para instalação do programa.",
+        "d": "Execução das instruções contidas em um script contendo o código-fonte, de forma sequencial.",
+        "e": "Organização do código-fonte em uma pasta contendo arquivos em ordem alfabética",
+        "r": "b"
+    },
+    {
+        "id": "8",
+        "p": "Precisamos de um vetor (array unidimensional) para armazenar 15 valores numéricos relativos aos salários de funcionários de uma empresa. Qual das seguintes instruções pode ser empregada para esse fim em um algoritmo?",
+        "a": "salarios : vetor [0..15] de real",
+        "b": "salarios : vetor [1..15] de numeros",
+        "c": "salarios : vetor [0..14] de salario",
+        "d": "salarios : vetor [0..14] de inteiro",
+        "e": "salarios : vetor [0..14] de real",
+        "r": "e"
+    },
+    {
+        "id": "9",
+        "p": "Qual dos seguintes itens mostra um laço for (para) escrito corretamente?",
+        "a": "para (con = 1; con < 10; con–) { instruções }",
+        "b": "para (con = 10; con >= 1; con++) { instruções }",
+        "c": "para (con = 10; con < 10; con–) { instruções }",
+        "d": "para (con = 1; con <= 10; con++) { instruções }",
+        "e": "para (con == 10; con > 1; con–) { instruções }",
+        "r": "d"
+    },
+    {
+        "id": "10",
+        "p": "O que é uma Variável?",
+        "a": "Local na memória CMOS do computador empregado para armazenar de forma temporária os dados que são utilizados pelo programa.",
+        "b": "Valor que varia conforme passa o tempo, sendo sempre modificado conforme o programa vai sendo executado.",
+        "c": "Estado lógico de uma constante, que pode variar entre verdadeiro e falso",
+        "d": "Local na memória RAM do computador utilizado para armazenar temporariamente dados que são utilizados pelo programa.",
+        "e": "Valor armazenado em locais variáveis no computador, como HD, RAM ou Memória Cache.",
         "r": "d"
     }
 ]
@@ -95,6 +170,8 @@ function telaDeResultado() {
     quizDiv.classList.add('escondido')
     main.classList.add('escondido')
     resultadoDiv.classList.remove('escondido')
+    resultado.textContent = pontos
+    tamanho.textContent = perguntasObj.length
 }
 
 function resetaAlt() {
